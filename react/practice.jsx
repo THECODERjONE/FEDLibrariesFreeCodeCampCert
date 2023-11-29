f77ba798f51f5dc6a87ccb5406d283fee2391758
THECODERjONE <jmmccode@gmail.com>
2023-11-29 22:44:22 +0100

    Update practice.jsx

--- react/practice.jsx
+++ react/practice.jsx
@@ -622,8 +622,7 @@ class ControlledInput extends React.Component {
     this.setState({
       input: event.target.value
     })
-  }
-  
+  }  
   // Change code above this line
   render() {
     return (
@@ -657,7 +656,10 @@ class MyForm extends React.Component {
   }
   handleSubmit(event) {
     // Change code below this line
-
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
     // Change code above this line
   }
   render() {
@@ -665,15 +667,193 @@ class MyForm extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           {/* Change code below this line */}
-
+          <input type="text" value={this.state.input} onChange={this.handleChange}/>
           {/* Change code above this line */}
           <button type='submit'>Submit!</button>
         </form>
         {/* Change code below this line */}
-
+        <h1>{this.state.submit}</h1>
         {/* Change code above this line */}
       </div>
     );
   }
 }
 /* Create a Controlled Form */
+
+/* Pass State as Props to Child Components */
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {/* Change code below this line */}
+         <Navbar name={this.state.name} />
+         {/* Change code above this line */}
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/* Change code below this line */}
+      <h1>Hello, my name is: {this.props.name}</h1>
+      {/* Change code above this line */}
+    </div>
+    );
+  }
+};
+/* Pass State as Props to Child Components */
+
+/* Pass a Callback as Props */
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        { /* Change code below this line */ }
+        <GetInput 
+          input={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        <RenderInput 
+        input={this.state.inputValue}
+        />
+        { /* Change code above this line */ }
+       </div>
+    );
+  }
+};
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
+/* Pass a Callback as Props */
+
+/* Use the Lifecycle Method componentWillMount */
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log("Save le Gecco")
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
+/* Use the Lifecycle Method componentWillMount */
+
+/* Use the Lifecycle Method componentDidMount */
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+/* Use the Lifecycle Method componentDidMount */
+
+/* Add Event Listeners */
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // Change code below this line
+  componentDidMount() {
+
+  }
+  componentWillUnmount() {
+
+  }
+  // Change code above this line
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
+/* Add Event Listeners */
