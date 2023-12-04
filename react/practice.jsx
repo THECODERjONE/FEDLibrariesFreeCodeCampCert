7c0a3b49952cfffb071530d03268682215987675
THECODERjONE <jmmccode@gmail.com>
2023-12-05 00:50:14 +0100

    Update practice.jsx

--- react/practice.jsx
+++ react/practice.jsx
@@ -931,77 +931,79 @@ class Colorful extends React.Component {
 /* Add Inline Styles in React */
 
 /* Use Advanced JavaScript in React Render Method */
-const inputStyle = {
-  width: 235,
-  margin: 5
-};
-
-class MagicEightBall extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: '',
-      randomIndex: ''
-    };
-    this.ask = this.ask.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  ask() {
-    if (this.state.userInput) {
+while (false) {
+  const inputStyle = {
+    width: 235,
+    margin: 5
+  };
+  
+  class MagicEightBall extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        userInput: '',
+        randomIndex: ''
+      };
+      this.ask = this.ask.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+    ask() {
+      if (this.state.userInput) {
+        this.setState({
+          randomIndex: Math.floor(Math.random() * 20),
+          userInput: ''
+        });
+      }
+    }
+    handleChange(event) {
       this.setState({
-        randomIndex: Math.floor(Math.random() * 20),
-        userInput: ''
+        userInput: event.target.value
       });
     }
-  }
-  handleChange(event) {
-    this.setState({
-      userInput: event.target.value
-    });
-  }
-  render() {
-    const possibleAnswers = [
-      'It is certain',
-      'It is decidedly so',
-      'Without a doubt',
-      'Yes, definitely',
-      'You may rely on it',
-      'As I see it, yes',
-      'Outlook good',
-      'Yes',
-      'Signs point to yes',
-      'Reply hazy try again',
-      'Ask again later',
-      'Better not tell you now',
-      'Cannot predict now',
-      'Concentrate and ask again',
-      "Don't count on it",
-      'My reply is no',
-      'My sources say no',
-      'Most likely',
-      'Outlook not so good',
-      'Very doubtful'
-    ];
-    const answer = possibleAnswers[this.state.randomIndex]; // Change this line
-    return (
-      <div>
-        <input
-          type='text'
-          value={this.state.userInput}
-          onChange={this.handleChange}
-          style={inputStyle}
-        />
-        <br />
-        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
-        <br />
-        <h3>Answer:</h3>
-        <p>
-          {/* Change code below this line */}
-          {answer}
-          {/* Change code above this line */}
-        </p>
-      </div>
-    );
+    render() {
+      const possibleAnswers = [
+        'It is certain',
+        'It is decidedly so',
+        'Without a doubt',
+        'Yes, definitely',
+        'You may rely on it',
+        'As I see it, yes',
+        'Outlook good',
+        'Yes',
+        'Signs point to yes',
+        'Reply hazy try again',
+        'Ask again later',
+        'Better not tell you now',
+        'Cannot predict now',
+        'Concentrate and ask again',
+        "Don't count on it",
+        'My reply is no',
+        'My sources say no',
+        'Most likely',
+        'Outlook not so good',
+        'Very doubtful'
+      ];
+      const answer = possibleAnswers[this.state.randomIndex]; // Change this line
+      return (
+        <div>
+          <input
+            type='text'
+            value={this.state.userInput}
+            onChange={this.handleChange}
+            style={inputStyle}
+          />
+          <br />
+          <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+          <br />
+          <h3>Answer:</h3>
+          <p>
+            {/* Change code below this line */}
+            {answer}
+            {/* Change code above this line */}
+          </p>
+        </div>
+      );
+    }
   }
 }
 /* Use Advanced JavaScript in React Render Method */
@@ -1057,11 +1059,60 @@ class MyComponent extends React.Component {
   render() {
     // Change code below this line
     return (
-       <div>
-         <button onClick={this.toggleDisplay}>Toggle Display</button>
-         <h1>Displayed!</h1>
-       </div>
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
     );
   }
 };
 /* Use && for a More Concise Conditional */
+
+/* Use a Ternary Expression for Conditional Rendering */
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {/* Change code below this line */}
+
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+/* Use a Ternary Expression for Conditional Rendering */
