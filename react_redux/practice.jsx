587d586d49a3dbdc0de0fbf1a3f50cc56688047d
THECODERjONE <jmmccode@gmail.com>
2023-12-11 22:15:22 +0100

    Create practice.jsx

---
+++ react_redux/practice.jsx
@@ -0,0 +1,10 @@
+/* Getting Started with React Redux */
+class DisplayMessages extends React.Component {
+    // Change code below this line
+    
+    // Change code above this line
+    render() {
+      return <div />
+    }
+  };
+/* Getting Started with React Redux */
